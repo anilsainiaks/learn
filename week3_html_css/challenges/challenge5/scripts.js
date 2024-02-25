99ebf67ab5ad86c2c33db224651322fb55454a04
anil saini <anil@techuz.com>
2024-02-25 20:26:03 +0530

    sun eve

---
+++ week3_html_css/challenges/challenge5/scripts.js
@@ -0,0 +1,10 @@
+function toggleMenu(){
+    const menuBtn=document.querySelector('.menu-icon');
+    const navlink=document.querySelector('.nav-links')
+    navlink.classList.toggle('mobile-menu')
+    // menuBtn.addEventListener('click',()=>{
+    //     navlink.classList.toggle('mobile-menu')
+    // })
+
+
+}
